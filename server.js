50ad98a054fd812806c75f42d945c14976fcca65
Ilya <ilyakopeysk@gmail.com>
2019-12-08 15:11:23 +0300

    simple return message

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const path           = require('path')
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const cors           = require('cors');
-const app            = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-const port = 8080;
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
-mongoClient.connect((err, database) => {
-    if (err) return console.log(err)
-    require('./app/routes')(app, database);
-    app.listen(port, () => {
-      console.log('We are live on ' + port);
-    });               
-  })
+app.listen(port, () => {
+    require('./app/routes')(app);
+    console.log(`App running on port ${port}.`)
+});
