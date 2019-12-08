23fa6d7620e5ce99773b70e2342fc1a95f7d8689
Ilya <ilyakopeysk@gmail.com>
2019-12-08 19:19:44 +0300

    added weather back

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,6 @@
 const db = require('../../db');
+const axios = require('axios');
+const APIKey = 'e34c51f8b71bca2945c6901e59dc5b69';
 module.exports = function(app) {
   app.get('/', (request, response) => {
     response.json({ info: 'Welcome to the server' })
@@ -18,5 +20,27 @@ module.exports = function(app) {
 
   app.delete('/favourites/:name',  (request, response) => {
     db.deleteFavourite(request, response);
+  });
+
+  app.get('/weather/city/:town', async (request, response) => {
+    try {
+      const { town } = request.params;
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${town}&APPID=${APIKey}`);
+      response.json(result.data);
+    } catch (e) {
+      console.log(e);
+      response.end();
+    }
+  });
+
+  app.get('/weather/coordinates/:long&:lat', async (request, response) => {
+    try {
+      const { lat, long } = request.params;
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${APIKey}`);
+      response.json(result.data);
+    } catch (e) {
+      console.log(e);
+      response.end();
+    }
   });
 };
